c209e63cdb3301fa276a76ca09492a1c6b1dcd30
TTtangtaocode <1351418289@qq.com>
2019-12-27 17:06:24 +0800

    添加websocket demon

--- kafka-stream/src/test/java/tang/tao/test/CommonKafkaStreamTest.java
+++ kafka-stream/src/test/java/tang/tao/test/CommonKafkaStreamTest.java
@@ -1,11 +1,34 @@
 package tang.tao.test;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class CommonKafkaStreamTest {
 
+    @Test
+    public void streamTest() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-stream-processing-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        StreamsBuilder builder = new StreamsBuilder();
+        builder.<String, String>stream("my-input-topic")
+                .mapValues(value -> value.length())
+                .to("my-output-topic");
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+    }
 }
